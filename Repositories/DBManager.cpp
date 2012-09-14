e277added73e91115482e2ea2e9a05b4f6fbb816
Ciprian Filipas <cipri@mosync.com>
2012-09-14 16:16:09 +0300

    code estetics and comments for the DBManager. Logical side to go

--- Repositories/DBManager.cpp
+++ Repositories/DBManager.cpp
@@ -14,6 +14,9 @@
 
 namespace Repositories
 {
+	/**
+	 * \brief Constructor
+	 */
 	DBManager::DBManager()
 	{
 		_DBExpenses = maFileOpen(EXPENSES_FILE, MA_ACCESS_READ_WRITE);
@@ -35,6 +38,9 @@ namespace Repositories
 		_consumedBudget = 0.0;
 	}
 
+	/**
+	 * \brief Destructor
+	 */
 	DBManager::~DBManager()
 	{
 		for(int i = 0; i <= _expenses->size(); i++)
@@ -47,6 +53,9 @@ namespace Repositories
 		delete _incomesFileContent;
 	}
 
+	/**
+	 * \brief This function is used for creating/reading the expenses file
+	 */
 	void DBManager::readExpenses()
 	{
 		_DBExpenses = maFileOpen(EXPENSES_FILE, MA_ACCESS_READ);
@@ -129,6 +138,9 @@ namespace Repositories
 		_expensesFileContent = NULL;
 	}
 
+	/**
+	 * \brief This function is used for creating/reading the incomes file
+	 */
 	void DBManager::readIncomes()
 	{
 		_DBIncomes = maFileOpen(INCOMES_FILE, MA_ACCESS_READ);
@@ -214,11 +226,29 @@ namespace Repositories
 		_incomesFileContent = NULL;
 	}
 
+	/**
+	 * \brief Getter for the number of expenses currently loaded
+	 * @return int
+	 */
 	int DBManager::getNumberOfExpenses() const
 	{
 		return _expenses->size();
 	}
 
+	/**
+	 * \brief Getter for the number of incomes currently loaded
+	 * @return int
+	 */
+	int DBManager::getNumberOfIncomes() const
+	{
+		return _incomes->size();
+	}
+
+	/**
+	 * \brief Gets the ExpenseObject placed at the position index inside the list of expenses
+	 * @param index const int& the index of the ExpenseObject
+	 * @return Model::ExpenseObject&
+	 */
 	Model::ExpenseObject& DBManager::getExpenseObject(const int& index) const
 	{
 		if(index < _expenses->size())
@@ -227,6 +257,24 @@ namespace Repositories
 		}
 	}
 
+	/**
+	 * \brief Gets the IncomeObject placed at the position index inside the list of incomes
+	 * @param index const int& the index of the IncomeObject
+	 * @return Model::IncomeObject&
+	 */
+	Model::IncomeObject& DBManager::getIncomeObject(const int& index) const
+	{
+		if(index < _incomes->size())
+		{
+			return (*(*_incomes)[index]);
+		}
+	}
+
+	/**
+	 * \brief This function creates a string representation of the expense object
+	 * 	      and appends it to the expenses file
+	 * @param obj Model::ExpenseObject* the expense object
+	 */
 	void DBManager::addExpense(Model::ExpenseObject* obj)
 	{
 		MAUtil::String expenseContent;
@@ -254,6 +302,11 @@ namespace Repositories
 		_consumedBudget += (obj->getAmount());
 	}
 
+	/**
+	 * \brief This function creates a string representation of the income object
+	 * 	      and appends it to the incomes file
+	 * @param obj Model::IncomeObject* the incomes object
+	 */
 	void DBManager::addIncome(Model::IncomeObject* obj)
 	{
 		MAUtil::String incomeContent;
@@ -281,32 +334,10 @@ namespace Repositories
 		_totalBudget += (obj->getAmount());
 	}
 
-	int DBManager::getNumberOfIncomes() const
-	{
-		return _incomes->size();
-	}
-
-	Model::IncomeObject& DBManager::getIncomeObject(const int& index) const
-	{
-		if(index < _incomes->size())
-		{
-			return (*(*_incomes)[index]);
-		}
-	}
-
-	void DBManager::addExpenseToList(const double& amount, const MAUtil::String& category, const MAUtil::String& description, const MAUtil::String& imgPath,
-								  const int& year, const int& month, const int& day, const int& hour, const int& minute)
-	{
-		Model::ExpenseObject* obj = new Model::ExpenseObject(amount, category, description, imgPath, year, month, day, hour, minute);
-		_expenses->add(obj);
-	}
-	void DBManager::addIncomeToList(const double& amount, const MAUtil::String& type, const MAUtil::String& description, const MAUtil::String& transactionInfo,
-								  const int& year, const int& month, const int& day, const int& hour, const int& minute)
-	{
-		Model::IncomeObject* obj = new Model::IncomeObject(amount, type, description, transactionInfo, year, month, day, hour, minute);
-		_incomes->add(obj);
-	}
-
+	/**
+	 * \brief This function returns the total budget value (the amount of all the incomes)
+	 * @return double
+	 */
 	double DBManager::getTotalBudget()
 	{
 		_totalBudget = 0.0;
@@ -320,6 +351,10 @@ namespace Repositories
 		return _totalBudget;
 	}
 
+	/**
+	 * \brief This function returns the consumed budget value (the amount of all the expenses)
+	 * @return double
+	 */
 	double DBManager::getConsumedBudget()
 	{
 		_consumedBudget = 0.0;
@@ -334,6 +369,11 @@ namespace Repositories
 		return _consumedBudget;
 	}
 
+	/**
+	 * \brief This function returns the amount of money spent for each category of expenses
+	 * @param category const MAUtil::String& the category string
+	 * @return double
+	 */
 	double DBManager::getCategoryAmount(const MAUtil::String& category)
 	{
 		double value = 0.0;
@@ -349,6 +389,28 @@ namespace Repositories
 		return value;
 	}
 
+	/**
+	 * \brief This function returns a pointer to the expenses list
+	 * @return MAUtil::Vector<Model::ExpenseObject*>*
+	 */
+	MAUtil::Vector<Model::ExpenseObject*>* DBManager::getExpenses()
+	{
+		return _expenses;
+	}
+
+	/**
+	 * \brief This function returns a pointer to the incomes list
+	 * @return MAUtil::Vector<Model::IncomeObject*>*
+	 */
+	MAUtil::Vector<Model::IncomeObject*>* DBManager::getIncomes()
+	{
+		return _incomes;
+	}
+
+	/**
+	 * \brief This function is used for setting the reference date
+	 * @param d const Model::DateStruct& the reference date
+	 */
 	void DBManager::setDate(const Model::DateStruct& d)
 	{
 		_date._day = d._day;
@@ -356,15 +418,9 @@ namespace Repositories
 		_date._year = d._year;
 	}
 
-	MAUtil::Vector<Model::ExpenseObject*>* DBManager::getExpenses()
-	{
-		return _expenses;
-	}
-	MAUtil::Vector<Model::IncomeObject*>* DBManager::getIncomes()
-	{
-		return _incomes;
-	}
-
+	/**
+	 * \brief This function is used for clearing the
+	 */
 	void DBManager::clearFiles()
 	{
 		_DBIncomes = maFileOpen(INCOMES_FILE, MA_ACCESS_READ_WRITE);
@@ -377,5 +433,43 @@ namespace Repositories
 
 		_incomes->clear();
 		_expenses->clear();
+	}
+
+	/**
+	 * \brief This function is used for adding an expense object to the list of expenses
+	 * @param amount const double& the amount value
+	 * @param category const MAUtil::String& the category value
+	 * @param description const MAUtil::String& the description value
+	 * @param imgPath const MAUtil::String& the image path value
+	 * @param year const int& the year value
+	 * @param month const int& the month value
+	 * @param day const int& the day value
+	 * @param hour const int& the hours value
+	 * @param minute const int& the minutes value
+	 */
+	void DBManager::addExpenseToList(const double& amount, const MAUtil::String& category, const MAUtil::String& description, const MAUtil::String& imgPath,
+								  const int& year, const int& month, const int& day, const int& hour, const int& minute)
+	{
+		Model::ExpenseObject* obj = new Model::ExpenseObject(amount, category, description, imgPath, year, month, day, hour, minute);
+		_expenses->add(obj);
+	}
+
+	/**
+	 * \brief This function is used for adding an income object to the list of incomes
+	 * @param amount const double& the amount value
+	 * @param type const MAUtil::String& the type value
+	 * @param description const MAUtil::String& the description value
+	 * @param transactionInfo const MAUtil::String& the transactionInfo value
+	 * @param year const int& the year value
+	 * @param month const int& the month value
+	 * @param day const int& the day value
+	 * @param hour const int& the hours value
+	 * @param minute const int& the minutes value
+	 */
+	void DBManager::addIncomeToList(const double& amount, const MAUtil::String& type, const MAUtil::String& description, const MAUtil::String& transactionInfo,
+								  const int& year, const int& month, const int& day, const int& hour, const int& minute)
+	{
+		Model::IncomeObject* obj = new Model::IncomeObject(amount, type, description, transactionInfo, year, month, day, hour, minute);
+		_incomes->add(obj);
 	}
 }
--- Repositories/DBManager.h
+++ Repositories/DBManager.h
@@ -25,40 +25,154 @@ namespace Repositories
 	class DBManager
 	{
 	public:
+		/**
+		 * \brief Constructor
+		 */
 		DBManager();
+
+		/**
+		 * \brief Destructor
+		 */
 		~DBManager();
 
+		/**
+		 * \brief This function is used for creating/reading the expenses file
+		 */
 		void readExpenses();
+
+		/**
+		 * \brief This function is used for creating/reading the incomes file
+		 */
 		void readIncomes();
+
+		/**
+		 * \brief Getter for the number of expenses currently loaded
+		 * @return int
+		 */
 		int getNumberOfExpenses() const;
+
+		/**
+		 * \brief Getter for the number of incomes currently loaded
+		 * @return int
+		 */
 		int getNumberOfIncomes() const;
+
+		/**
+		 * \brief Gets the ExpenseObject placed at the position index inside the list of expenses
+		 * @param index const int& the index of the ExpenseObject
+		 * @return Model::ExpenseObject&
+		 */
 		Model::ExpenseObject& getExpenseObject(const int& index) const;
+
+		/**
+		 * \brief Gets the IncomeObject placed at the position index inside the list of incomes
+		 * @param index const int& the index of the IncomeObject
+		 * @return Model::IncomeObject&
+		 */
 		Model::IncomeObject& getIncomeObject(const int& index) const;
+
+		/**
+		 * \brief This function creates a string representation of the expense object
+		 * 	      and appends it to the expenses file
+		 * @param obj Model::ExpenseObject* the expense object
+		 */
 		void addExpense(Model::ExpenseObject* obj);
+
+		/**
+		 * \brief This function creates a string representation of the income object
+		 * 	      and appends it to the incomes file
+		 * @param obj Model::IncomeObject* the incomes object
+		 */
 		void addIncome(Model::IncomeObject* obj);
+
+		/**
+		 * \brief This function returns the total budget value (the amount of all the incomes)
+		 * @return double
+		 */
 		double getTotalBudget();
+
+		/**
+		 * \brief This function returns the consumed budget value (the amount of all the expenses)
+		 * @return double
+		 */
 		double getConsumedBudget();
+
+		/**
+		 * \brief This function returns the amount of money spent for each category of expenses
+		 * @param category const MAUtil::String& the category string
+		 * @return double
+		 */
 		double getCategoryAmount(const MAUtil::String& category);
+
+		/**
+		 * \brief This function returns a pointer to the expenses list
+		 * @return MAUtil::Vector<Model::ExpenseObject*>*
+		 */
 		MAUtil::Vector<Model::ExpenseObject*>* getExpenses();
+
+		/**
+		 * \brief This function returns a pointer to the incomes list
+		 * @return MAUtil::Vector<Model::IncomeObject*>*
+		 */
 		MAUtil::Vector<Model::IncomeObject*>* getIncomes();
 
+		/**
+		 * \brief This function is used for setting the reference date
+		 * @param d const Model::DateStruct& the reference date
+		 */
 		void setDate(const Model::DateStruct& d);
 
+		/**
+		 * \brief This function is used for clearing the
+		 */
 		void clearFiles();
 	private:
+		/**
+		 * \brief This function is used for adding an expense object to the list of expenses
+		 * @param amount const double& the amount value
+		 * @param category const MAUtil::String& the category value
+		 * @param description const MAUtil::String& the description value
+		 * @param imgPath const MAUtil::String& the image path value
+		 * @param year const int& the year value
+		 * @param month const int& the month value
+		 * @param day const int& the day value
+		 * @param hour const int& the hours value
+		 * @param minute const int& the minutes value
+		 */
 		void addExpenseToList(const double& amount, const MAUtil::String& category, const MAUtil::String& description, const MAUtil::String& imgPath,
 							  const int& year, const int& month, const int& day, const int& hour, const int& minute);
+
+		/**
+		 * \brief This function is used for adding an income object to the list of incomes
+		 * @param amount const double& the amount value
+		 * @param type const MAUtil::String& the type value
+		 * @param description const MAUtil::String& the description value
+		 * @param transactionInfo const MAUtil::String& the transactionInfo value
+		 * @param year const int& the year value
+		 * @param month const int& the month value
+		 * @param day const int& the day value
+		 * @param hour const int& the hours value
+		 * @param minute const int& the minutes value
+		 */
 		void addIncomeToList(const double& amount, const MAUtil::String& type, const MAUtil::String& description, const MAUtil::String& transactionInfo,
 									  const int& year, const int& month, const int& day, const int& hour, const int& minute);
+		// The file handles
 		MAHandle _DBExpenses;
 		MAHandle _DBIncomes;
+
+		// The expenses/incomes lists
 		MAUtil::Vector<Model::ExpenseObject*>* _expenses;
 		MAUtil::Vector<Model::IncomeObject*>* _incomes;
+
+		// The file contents are loaded into these variables
 		MAUtil::String* _expensesFileContent;
 		MAUtil::String* _incomesFileContent;
+
+		// The budget values
 		double _totalBudget;
 		double _consumedBudget;
 
+		// The reference date
 		Model::DateStruct _date;
 	};
 }
